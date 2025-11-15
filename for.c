ed39c6d2130d67b546a4d0d65da82790823a9323
Nagashri05 <nagashri33@gmail.com>
2025-11-15 14:44:50 +0530

    practicing for loop
    learning for  loop

---
+++ for.c
@@ -0,0 +1,126 @@
+#include<stdio.h>
+
+int main(){
+    int i;
+    // printf("enter the number of times to print given message:");
+    // scanf("%d",&n);
+
+    //1.print numbers from 0 to 9
+//    for (i=0;i<10;i++){
+//     printf("%d\n",i);
+//    }
+
+    //2.print even from 2 to 20.
+
+
+    // printf("even numbers are:");
+    // for(int i=0;i<=n;i=i+2){
+    //    printf(" %d\n ",i);
+    //     }
+
+        //3/PRINT ODD NUMBERS
+    // printf("odd numbers are:");
+    // for (int i=1;i<=n;i=i+2){
+    //     printf("%d\n",i);
+    // }
+
+    //4.Write a for loop that prints numbers 1 to 3.
+    // for(i=1;i<=3;i++){
+    //     printf("%d\n",i);
+    // }
+
+    //5.Write a for loop that prints the word "Hello" 5 times.
+
+    // for(i=1;i<=5;i++){
+    //     printf("Hello\n");
+    // }
+
+    //6. Write a for loop that prints numbers 1, 2, 3, 4, 5 each on a new line.
+//    for(i=1;i<=5;i++){
+//     printf("%d\n",i);
+//    }
+
+//7.Write a for loop that prints numbers 10 down to 6
+// for(i=10;i>=6;i--){
+//     printf("%d\n",i);
+// }
+
+    //Write a for loop that prints only even numbers from 2 to 8.
+    // for(i=2;i<=8;i=i+2){
+    //     printf("%d\n",i);
+    // }
+
+    //8.Write a for loop that prints only odd numbers from 1 to 7.
+    // for (int i=1;i<=7;i=i+2){
+    //     printf("%d\n",i);
+    // }
+
+    // 9.Write a for loop that prints the number 7, four times.
+    // for(i=1;i<=4;i++){
+    //     printf("7\n");
+    // }
+
+
+    //10.Write a for loop that prints the square of numbers from 1 to 4.
+// for(i=1;i<=4;i++){
+//     printf("%d\n",i*i);
+// }
+
+
+//11.Write a for loop that prints:
+// for(i=1;i<=3;i++){
+//     printf("*\n");
+// }
+
+    //12.Write a for loop that prints:
+    //1 1 1
+   
+    // for(i=1;i<=3;i++){
+    //     printf("1 ");
+    // }
+
+  //13.Print numbers from 5 to 1 (reverse order).  
+//   for(int i=5;i>=1;i--){
+//     printf("%d\n",i);
+//   }
+    //14.Print the first 10 even numbers.
+    // for(i=0;i<20;i=i+2){
+    //     printf("%d\n",i);
+    // }
+
+    //15.Print the first 10 odd numbers.
+    // for(i=1;i<20;i=i+2){
+    //     printf("%d\n",i);
+    //    }
+
+    //16.Print the multiplication table of 5 using a for loop
+    // for (i=1;i<=10;i++){
+    //     printf("5x%d=%d\n",i,5*i);
+    // }
+
+    // 17.Print all numbers from 1 to n (input from user)
+    //int n;
+
+    // printf("enter the value for n:");
+    // scanf("%d",&n);
+
+    // for(i=1;i<=n;i++){
+    //     printf("%d\n",i);
+    // }
+
+    //18.Print this pattern:
+
+// *
+// **
+// ***
+// ****
+
+// for(i=1;i<=4;i++){
+//     for(int j=1;j<=i;j++){
+//         printf("* ");
+//     }
+//     printf("\n");
+// }
+
+    return 0;
+}
